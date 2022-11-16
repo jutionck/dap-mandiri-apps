27c2f9381a7edcfcf164c7d4e2d69c63a2001b17
jutionck <jutionck@gmail.com>
2022-11-16 13:43:14 +0700

    add: map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeModule } from './pages/home/home.module';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
 
