d60afffe2a47c2f475e59f8224f192d0e9bb0158
jutionck <jutionck@gmail.com>
2022-11-17 08:43:26 +0700

    add: bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoursesModule } from './pages/courses/courses.module';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
 
@@ -11,6 +12,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     SharedModule,
     PagesModule,
+    CoursesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
