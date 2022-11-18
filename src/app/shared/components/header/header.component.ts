62a49b1699873160a95976bbea9d465eb82ea436
jutionck <jutionck@gmail.com>
2022-11-18 14:17:26 +0700

    add: scroll to each section on homepage

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  scrollToElement(element: string): void {
+    document
+      .querySelector(element)!
+      .scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 }
